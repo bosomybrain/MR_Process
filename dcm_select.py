d8e99a80455e13e7b40593adf28b55d1c7873952
bittersweetLiu <158543549+bittersweetLiu@users.noreply.github.com>
2024-02-03 14:33:57 +0800

    Add files via upload

--- dcm_select.py
+++ dcm_select.py
@@ -1,6 +1,27 @@
 import os
 import shutil
+
+import pydicom
+
 import whether_have_nii
+'''
+判断一个列表中的数字是否连续
+'''
+def is_continuous(lst):
+    #如果列表为字符串，则转化为int
+    if isinstance(lst[0], str):
+        lst = [int(x) for x in lst]
+    # 将列表转换为集合
+    s = set(lst)
+    # 如果集合中的元素数量不等于列表长度，说明有重复元素
+    if len(s) != len(lst):
+        return False
+    # 如果集合中的最小值和最大值之间的元素数量不等于列表长度，说明有缺失元素
+    elif max(s) - min(s) + 1 != len(lst):
+        return False
+    # 如果以上两个条件都不满足，则说明列表是连续的自然数
+    else:
+        return True
 
 import nibabel as nib
 '''
@@ -59,104 +80,71 @@ output:none
 '''
 def dcm_select_labeled(root_path,output_path,target_name):
     txt_file = open('dcm_select_exception_log.txt', 'w')
-    dcm_dir_id = ["701015", "601015", "401015"]
+    root_name = root_path.split('\\')[1]
     for root, dir, file in os.walk(root_path):
 
-        #dcm_nii_match_end = False  #
-        #patient_root = ''
-        split_root = root.split('\\')  # 通过\\来进行截断
-
-        if target_name in file :
-            label_nii_exist = True
-            #print('dir',dir)
-            #print('root',root)
-            nii_label_addr = os.path.join(root, target_name)
-            nii_label = nib.load(nii_label_addr).get_fdata()
-            (x,y,z) = nii_label.shape
-            #print(x,y,z)
         if len(root.split('\\')) == 3: # 找到每个病例的根目录地址
-            print(root)
+            num_T2W_TSE = 0
+            #print(root)
             patient_root = root
             label_nii_exist = whether_have_nii.whether_have_nii(patient_root, target_name)
-        if len(root.split('\\')) == 4:  # 找到每个病例的根目录地址
-            patient_name = root.split('\\')[2]
-            dcm_dir_match_num = 0
-            for id in dcm_dir_id:
+            if not label_nii_exist:
+                txt_file.write('{} has no label'.format(patient_root) + '\n')
+                print('{} has no label!!!!!!!!!!'.format(patient_root))
+                shutil.move(patient_root, patient_root.replace(root_name, 'exception_patient/no_label_patient'))
+        if len(root.split('\\')) == 4:  # 判断T2W_TSE文件夹数目
+            for d in dir:
+                d_path = os.path.join(root, d)
+                #print(d_path)
+                sample_f  = os.listdir(d_path)[0]
+                #print(sample_f)
+                sample_f_old_path = os.path.join(d_path, sample_f)
+                sample_f_dcm_data = pydicom.read_file(sample_f_old_path)
+                sample_f_SeriesDescription = sample_f_dcm_data.SeriesDescription
+                if sample_f_SeriesDescription == 'T2W_TSE':
+                        num_T2W_TSE += 1
+            #print(num_T2W_TSE)
+            if num_T2W_TSE > 1:
+                txt_file.write('{} has more than one T2W_TSE dir!!!!!!!!!!'.format(patient_root) + '\n')
 
-                #print(id)
-                dcm_dir_addr = os.path.join(root,id)
-                if os.path.exists(dcm_dir_addr):
-                    #print(dcm_dir_addr)
-                    print(id)
-                    dcm_files = os.listdir(dcm_dir_addr)
-                    if len(dcm_files)== z:
-                        dcm_dir_match_num += 1
-                        print(dcm_dir_match_num)
-                        match_id = id
+                print('{} has more than one T2W_TSE dir!!!!!!!!!!'.format(patient_root))
+                shutil.move(patient_root, patient_root.replace(root_name, 'exception_patient/other'))
 
-            print('final dcm_dir_match_num',dcm_dir_match_num)
-            if label_nii_exist  and dcm_dir_match_num == 1:
-                match_dcm_dir = os.path.join(root,match_id)
-                match_dcm_files = os.listdir(match_dcm_dir)
-                for match_dcm_file in match_dcm_files:
-                    split_file = match_dcm_file.split('_')
-                    f_old_path = os.path.join(match_dcm_dir, match_dcm_file)
-                    # 找到放了T2W_TSE的文件夹，复制里面的所有文件，并修改名称
-                    f_new_name = patient_name + '_T2W_TSE_' + split_file[-1]
-                    # 保存图片到新路径
-                    f_new_path = os.path.join(output_path, f_new_name)
+        #print(num_T2W_TSE)
+        if len(root.split('\\')) == 5:  # 找到dcm文件夹目录地址
+            if num_T2W_TSE == 1:
+                #print('yes')
+                patient_name = root.split('\\')[2]
+                sample_f = file[0]
+                sample_f_old_path = os.path.join(root, sample_f)
+                sample_f_dcm_data = pydicom.read_file(sample_f_old_path)
+                f_SeriesDescription = sample_f_dcm_data.SeriesDescription
+                if f_SeriesDescription == 'T2W_TSE': #dcm文件tag是否为 ‘T2W_TSE’
+                    # 找出dcm文件夹里的dcm文件数字标识
+                    file_digits = []
+                    for f in file:
+                        f_name = f.replace('.dcm', '')
+                        f_digit = f_name.split('_')[-1]
+                        file_digits.append(f_digit)
+                    file_digits.sort()
+                    # 找出dcm文件夹里的dcm文件数字标识
+                    if is_continuous(file_digits): # dcm数字连续
+                        for f in file:
+                            f_old_path = os.path.join(root, f)
+                            # 找到放了T2W_TSE的文件夹，复制里面的所有文件，并修改名称
+                            f_new_name = patient_name + '_T2W_TSE_' + f.split('_')[-1]
 
-                    shutil.copy(f_old_path, f_new_path)
-            else :  # dcm_nii匹配结束且没有找到dcm文件夹匹配nii标注
+                            # 保存图片到新路径
+                            f_new_path = os.path.join(output_path, f_new_name)
 
-                txt_file.write('{} dcm_dir_match_num: {}'.format(patient_root, dcm_dir_match_num) + '\n')
+                            shutil.copy(f_old_path, f_new_path)
+                    else :  # dcm数字不连续
 
-                print('{} dcm dir match error while dcm selecting!'.format(patient_root))
-                shutil.move(patient_root, patient_root.replace('patient_group', 'exception_patient'))
+                        txt_file.write('{} dcm files are not continuous!!!!!!!!!!!!!'.format(patient_root) + '\n')
 
+                        print('{} dcm files are not continuous!!!!!!!!!!!!!'.format(patient_root))
+                        shutil.move(patient_root, patient_root.replace(root_name, 'exception_patient/not_continuous'))
 
-
-        # if (split_root[-1]) == "701015" or (split_root[-1]) == "601015" or (split_root[-1]) == "401015":
-        #
-        #     dcm_nii_match_end = True
-        #     #print('root',root)
-        #     #print(len(file))
-        #     patient_name = root.split('\\')[-3]
-        #     if len(file) == z:  #dcm文件夹下数量和nii匹配
-        #         dcm_dir_match_num += 1 #dcm文件夹匹配数+1
-        #         print(split_root[-1], dcm_dir_match_num)
-        #         #assert dcm_dir_num_match == 1 or dcm_dir_num_match == 0, '{} dcm dir match error!'.format(patient_name)
-        #         if dcm_dir_match_num == 1 : #出现一个dcm文件夹匹配nii标注
-        #             root_patient = '.'
-        #             for i in range(1,len(split_root)-2):
-        #                 root_patient = root_patient + '/' + split_root[i]#判断该患者是否有nii
-        #             # print(root_patient)
-        #             answer = whether_have_nii.whether_have_nii(root_patient, target_name)
-        #             #print(root_patient + "地址患者的nii分割存在结果是：" )
-        #             #print(answer)
-        #             if answer == 1:
-        #                     for f in file:
-        #                         split_file = f.split('_')
-        #                         f_old_path = os.path.join(root, f)
-        #                         # 找到放了T2W_TSE的文件夹，复制里面的所有文件，并修改名称
-        #                         f_new_name = split_root[-3] + '_T2W_TSE_' + split_file[-1]
-        #                         # 保存图片到新路径
-        #                         f_new_path = os.path.join(output_path, f_new_name)
-        #                         #shutil.copy(f_old_path, f_new_path)
-        #         elif dcm_dir_match_num >1 :
-        #             txt_file.write('{} dcm_dir_match_num: {}'.format(patient_root, dcm_dir_match_num) + '\n')
-        #
-        #             print('{} dcm dir match error while dcm selecting!'.format(patient_root))
-        # #if dcm_nii_match_end == True:
-        #     #print(dcm_dir_match_num)
-        # if dcm_nii_match_end == True and dcm_dir_match_num == 0: #dcm_nii匹配结束且没有找到dcm文件夹匹配nii标注
-        #
-        #     txt_file.write('{} dcm_dir_match_num: {}'.format(patient_root, dcm_dir_match_num)+'\n')
-        #
-        #     print('{} dcm dir match error while dcm selecting!'.format(patient_root))
-        #     #print(patient_root)
-        #     #shutil.move(patient_root, patient_root.replace('patient_group', 'exception_patient'))
-        #     #shutil.move(patient_root, patient_root.replace('exception_patient', 'test1'))
     txt_file.close()
 
 
